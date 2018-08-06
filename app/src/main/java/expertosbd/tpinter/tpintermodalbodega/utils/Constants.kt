8118df844824ff93acb9d94be4a8138ae0cbc80a
Diego Barrena <diegogo1234@gmail.com>
2018-08-06 17:14:27 +0200

    Update Constants.kt

--- app/src/main/java/expertosbd/tpinter/tpintermodalbodega/utils/Constants.kt
+++ app/src/main/java/expertosbd/tpinter/tpintermodalbodega/utils/Constants.kt
@@ -1,4 +1,4 @@
 package expertosbd.tpinter.tpintermodalbodega.utils
 
 /** The base URL of the API */
-const val BASE_URL: String = "http://tpinter.dbxprts.com:8080/apex/tp_inter/bodega/"
+const val BASE_URL: String = /*REDACTED*/
